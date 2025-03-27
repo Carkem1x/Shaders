9089fc11774e9c00a10e93866c6b812f5602cf35
Coco School <aecg117@gmail.com>
2025-03-27 14:57:17 -0600

    Invisible

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xd();
+    }
+
+    /// <summary>
+    /// Esta funcion hace algo
+    /// </summary>
+    void xd()
+    {
+
+    }
+}
